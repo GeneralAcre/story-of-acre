5b025df101faded108c03fb6ce76f672dc909c90
sanpaphat14 <sanpaphat14@gmail.com>
2026-07-29 05:50:08 +0700

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Kdam_Thmor_Pro, Press_Start_2P } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import ClientShell from "./ClientShell";
 
@@ -39,6 +40,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${kdam.variable} ${pressStart2P.variable}`}>
       <body>
         <ClientShell>{children}</ClientShell>
+        <Analytics />
       </body>
     </html>
   );
